29c7614a704de7e8486d81555b14714fc5150583
dyonak <dyonak@gmail.com>
2015-08-29 13:25:25 -0500

    Implemented ServerManager Class
    app.py updates to implement servermanager class. Tested startup, update,
    messaging. All working.

--- ServerManager.py
+++ ServerManager.py
@@ -1,25 +1,27 @@
 import time, subprocess
 
-class ServerManager:
+
+class ServerManager(object):
 
     def __init__(self, path, server_file, xms=1, xmx=1, gui=False):
+        self.process = None
         self.path = path
         self.server_file = server_file
         self.online = False
         self.xms = xms
         self.xmx = xmx
         self.gui = gui
-        self.process = None
-
 
     def start(self):
-        command = 'java -jar -Xms' + str(self.xms) + 'G -Xmx' + str(self.xmx) + 'G ' + self.path + self.server_file
-        if self.gui == False: command += ' nogui'
-        print command
-        if self.path !='':self.process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, cwd=self.path)
-        if self.path =='':self.process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
+        command = 'java -jar -Xms' + str(self.xms) + 'G -Xmx' + str(self.xmx) + 'G ' + self.path + '' + self.server_file
+        if not self.gui: command += ' nogui'
+        print '--- Started server with command: ' + command
+        if self.path == '':
+            self.process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
+        else:
+            self.process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, cwd=self.path)
         self.online = True
-        return
+        print self.process
 
     def shutdown(self):
         self.message('New version detected, server will be shutting down for maintenance in 1 minute.')
@@ -28,26 +30,21 @@ class ServerManager:
         time.sleep(20)
         self.message('Shutting down for maintenance in 10 seconds.')
         self.message('Have some diamonds for the trouble.')
-        self.message('give @a minecraft:diamond 5', true)
+        self.message('give @a minecraft:diamond 5', True)
+        time.sleep(10)
+        self.message('stop', True)
         self.online = False
         self.process.terminate()
-        del process
-        return
+        time.sleep(5)
 
-    #Server chat can be passed as ServerManager.message('hello')
+    # Server chat can be passed as ServerManager.message('hello')
     def message(self, message, command=False):
-        
-        if command == False:
-            self.process.stdin.write('### SMSW Message: ')
+        print self.process
+        if not command:
+            self.process.stdin.write('say ### SMSW Message: \n')
             self.process.stdin.flush()
-            self.process.stdin.write(message)
+            self.process.stdin.write('say ' + message + '\n')
             self.process.stdin.flush()
         else:
-            self.process.stdin.write(message)
+            self.process.stdin.write(message + '\n')
             self.process.stdin.flush()
-        return
-            
-        
-        
-        
-        
--- app.py
+++ app.py
@@ -5,99 +5,96 @@
 # it stops the server, downloads the update and restarts with the newest version.
 
 import time, subprocess, json, urllib2, urllib, argparse, os, hashlib, pprint
+from ServerManager import ServerManager
 
 # Global Settings
 
 minecraft_version_url = 'https://s3.amazonaws.com/Minecraft.Download/versions/versions.json'
-check_for_new_versions_frequency = 3600 # every hour
-mc_server = 'minecraft_server.jar' # server file name
+check_for_new_versions_frequency = 20  # every hour
+mc_server = 'minecraft_server.jar'  # server file name
 current_ver = ''
 run = 0
 
+
 # Process command line arguments for memory settings
 def process_args():
-	global results
-	parser = argparse.ArgumentParser(description="Simple Minecraft Server Wrapper")
-	parser.add_argument("-m", action='store', dest='memmin', help="Sets the minimum/initial memory usage for the Minecraft server in GB (ex: 1, 2, 3, 4)", type=int, default=1)
-	parser.add_argument("-x", action='store', dest='memmax', help="Sets the maximum memory usage for the Minecraft server in GB (ex: 1, 2, 3, 4)", type=int, default=1)
-	parser.add_argument("-p", action='store', dest='path', help="Set the local path where you want the server downloaded and ran, default is local directory", type=str, default='')
-	parser.add_argument("-g", action='store_true', dest='gui', help="Utilize the Minecraft server GUI, default is off")
-	parser.add_argument("-d", action='store_true', dest='dev', help="Use development snapshots instead of stable Minecraft releases")
-	results = parser.parse_args()
-	if results.path != '': results.path = os.path.join(results.path, '') # Path handling to include the trailing slash
-   	return
+    global results
+    parser = argparse.ArgumentParser(description="Simple Minecraft Server Wrapper")
+    parser.add_argument("-m", action='store', dest='memmin',
+                        help="Sets the minimum/initial memory usage for the Minecraft server in GB (ex: 1, 2, 3, 4)",
+                        type=int, default=1)
+    parser.add_argument("-x", action='store', dest='memmax',
+                        help="Sets the maximum memory usage for the Minecraft server in GB (ex: 1, 2, 3, 4)", type=int,
+                        default=1)
+    parser.add_argument("-p", action='store', dest='path',
+                        help="Set the local path where you want the server downloaded and ran, default is local directory",
+                        type=str, default='')
+    parser.add_argument("-g", action='store_true', dest='gui', help="Utilize the Minecraft server GUI, default is off")
+    parser.add_argument("-d", action='store_true', dest='dev',
+                        help="Use development snapshots instead of stable Minecraft releases")
+    results = parser.parse_args()
+    if results.path != '': results.path = os.path.join(results.path, '')  # Path handling to include the trailing slash
+    return
 
 
 # Download the latest version JSON file for Minecraft and see what the latest version is
 def get_version():
-	source = urllib2.urlopen(minecraft_version_url)
-	data = json.load(source)
-	ver = data["latest"]["snapshot"] if results.dev == True else data["latest"]["release"]
-	print '--- The latest version of Minecraft is', ver
-	return ver
-
-def installed_version():
-        #current_version = hashlib.md5(results.path + mc_server)
-        #print current_version
-        return current_version
+    source = urllib2.urlopen(minecraft_version_url)
+    data = json.load(source)
+    ver = data["latest"]["snapshot"] if results.dev == True else data["latest"]["release"]
+    print '--- The latest version of Minecraft is', ver
+    return ver
 
 # Checks if the latest version matches current version.
 def up_to_date(current_ver):
-	temp_ver = str(get_version())
-	if current_ver != temp_ver:
-		print '--- New version detected.'
-		return False
-	else:
-		print '--- Up to date.'
-		return True
+    temp_ver = str(get_version())
+    if current_ver != temp_ver:
+        print '--- New version detected.'
+        return False
+    else:
+        print '--- Up to date.'
+        return True
 
 # Downloads and saves the latest minecraft server.
 def download_server(version):
-	jarfile = urllib.URLopener()
-	print "--- Downloading", version
-	jarfile.retrieve("https://s3.amazonaws.com/Minecraft.Download/versions/"+ version + "/minecraft_server." + version + ".jar", results.path + mc_server)
-	print "--- Download complete."
+    jarfile = urllib.URLopener()
+    print "--- Downloading", version
+    jarfile.retrieve(
+        "https://s3.amazonaws.com/Minecraft.Download/versions/" + version + "/minecraft_server." + version + ".jar",
+        results.path + mc_server)
+    print "--- Download complete."
 
 # Supervisor program
 def main():
-	process_args()
-	global current_ver
-	global run
-	print '*' * 40
-	print '* Simple Minecraft Server Wrapper'
-	print '*' * 40
-	latest_ver = str(get_version())
-	if current_ver != latest_ver:
-		download_server(latest_ver)
-		current_ver = latest_ver
-	if run == 0:
-		run = 1
-		command = 'java -jar -Xms' + str(results.memmin) + 'G -Xmx' + str(results.memmax) + 'G ' + results.path + mc_server
-		if results.gui == False: command += ' nogui'
-		print '--- Starting server with command: ' + command
-		if results.path == '':mc = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
-		if results.path != '':mc = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, cwd=results.path)
-		time.sleep(5)
-		while run == 1:
-			print '--- Checking for new versions in ' + str(check_for_new_versions_frequency) + ' seconds.'
-			time.sleep(check_for_new_versions_frequency)
-			print '--- Checking for a new version...'
-			mc.stdin.write('say SMSW - Checking for new version...\n')
-			mc.stdin.flush()
-			if up_to_date(current_ver) == False:
-				mc.stdin.write('say SMSW - New version detected, rebooting for update in 30 seconds...\n')
-				mc.stdin.flush()
-				mc.stdin.write('say SMSW - Sorry for the troubles, have a diamond.\n')
-				mc.stdin.flush()
-				mc.stdin.write('give @a minecraft:diamond 5\n')
-				mc.stdin.flush()
-				time.sleep(30)
-				mc.terminate()
-				run = 0
-				time.sleep(5)
-				print '--- Server stopped'
-				main()
+    process_args()
+    server = ServerManager(results.path, mc_server, results.memmin, results.memmax, results.gui)
+    global current_ver
+    global run
+    print '*' * 40
+    print '* Simple Minecraft Server Wrapper'
+    print '*' * 40
+    latest_ver = str(get_version())
+    if current_ver == latest_ver:
+        download_server(latest_ver)
+        current_ver = latest_ver
+    if not server.online:
+        server.start()
+        time.sleep(5)
+        while server.online:
+            print '--- Checking for new versions in ' + str(check_for_new_versions_frequency) + ' seconds.'
+            time.sleep(check_for_new_versions_frequency)
+            print '--- Checking for a new version...'
+            server.message('Checking for a new version...')
+            # Checking for new version
+            if not up_to_date(current_ver):
+                # new version detected
+                time.sleep(30)
+                server.shutdown()
+                del server
+                time.sleep(5)
+                print '--- Server stopped'
+                main()
 
 # Start when run.
 if __name__ == '__main__':
-	main()
+    main()
